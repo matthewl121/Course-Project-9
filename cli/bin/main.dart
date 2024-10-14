ed95a6007022ed628364198797b1b1d148280b27
Matthew Li <mli007@hawaiiantel.net>
2024-10-14 16:18:54 -0400

    test 4

--- cli/bin/main.dart
+++ cli/bin/main.dart
@@ -1,7 +1,5 @@
 import 'package:cli/lib/utilities/router.dart'; // ignore: uri_does_not_exist
 
-Router router = Router();
-
 void main(List<String> arguments) {
   router.parseArguments(arguments);
 }
